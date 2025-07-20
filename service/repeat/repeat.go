6b27bb38af06f6d27844ac09b2403258ca7e8e7e
PupZemli-code <m.i.s.h.a.9611@mail.ru>
2025-07-20 13:39:07 +0300

    Update repeat.go -добавлена обработка для "y"

--- service/repeat/repeat.go
+++ service/repeat/repeat.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// ValidDstarRepeat производит проверки входящих данных dstart, repeat
 func ValidDstarRepeat(dstart string, repeat string) error {
 	if repeat == "" {
 		return fmt.Errorf("в параметре repeat — пустая строка")
@@ -20,7 +21,7 @@ func ValidDstarRepeat(dstart string, repeat string) error {
 		}
 		interval, err := strconv.Atoi(formatRepeat[1])
 		if err != nil {
-			return err
+			return fmt.Errorf("ошибка форматирования strconv.Atoi(formatRepeat[1]): %w", err)
 		}
 		if interval > 400 {
 			return fmt.Errorf("первышено максимально допустимый интрервал для %v = %v", formatRepeat, interval)
@@ -38,31 +39,55 @@ func ValidDstarRepeat(dstart string, repeat string) error {
 		}
 	default:
 		if formatRepeat[0] != "m" && formatRepeat[0] != "y" {
-			fmt.Errorf("недопустимый символ формата: %v", formatRepeat[0])
+			return fmt.Errorf("недопустимый символ формата: %v", formatRepeat[0])
 		}
 	}
 	return nil
 }
 
+// NextDate возвращает строку с датой в формате 20060102
 func NextDate(now time.Time, dstart string, repeat string) (string, error) {
 	if err := ValidDstarRepeat(dstart, repeat); err != nil {
 		return "", fmt.Errorf("формат repeat не прошел проверку: %w", err)
 	}
-	/* // В разработке
+	// В разработке
 	date, err := time.Parse("20060102", dstart)
 	if err != nil {
 		return "", fmt.Errorf("время в переменной dstart не может быть преобразовано в корректную дату — ошибка выполнения time.Parse('20060102', dstart): %w", err)
 	}
 	formatRepeat := strings.Split(repeat, " ")
-	var interval int
+	var days int
 
-	for {
-		date = date.AddDate(0, 0, interval)
-		if afterNow(date, now) {
-			break
+	switch formatRepeat[0] {
+	case "d":
+		days, err = strconv.Atoi(formatRepeat[1])
+		if err != nil {
+			return "", fmt.Errorf("ошибка форматирования strconv.Atoi(formatRepeat[1]): %w", err)
 		}
+
+		for {
+			date = date.AddDate(0, 0, days)
+			if afterNow(date, now) {
+				break
+			}
+		}
+		return date.Format("20060102"), nil
+
+	case "w":
+		return "", fmt.Errorf("формат еще не подерживается")
+	case "m":
+		return "", fmt.Errorf("формат еще не подерживается")
+	case "y":
+		for {
+			date = date.AddDate(1, 0, 0)
+			if afterNow(date, now) {
+				break
+			}
+		}
+		return date.Format("20060102"), nil
+	default:
+		return "", nil
 	}
-	*/
 }
 
 func afterNow(date, now time.Time) bool {
